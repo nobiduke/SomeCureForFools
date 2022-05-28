f858d9d34077146360cca1022e7c46fca5ec3a52
nobiduke <lmjones647@gmail.com>
2022-05-28 15:49:38 -0400

    Match updated for readability

--- Match.py
+++ Match.py
@@ -11,21 +11,21 @@ from Player import Player
 class Match:
     def __init__(self, info) -> None:
 
-        self.gameId = info["gameId"]
-        self.startTimeStamp = info["gameStartTimestamp"]
-        self.duration = info["gameDuration"]
+        self.gameId = str(info["gameId"])
+        self.startTimeStamp = str(info["gameStartTimestamp"])
+        self.duration = str(info["gameDuration"])
         self.gameMode = info["gameMode"]
         self.mapId = info["mapId"]
-        self.teams = {"100": {}, "200": {}}
+        self.teams = {"100": [], "200": []}
         self.participants = {}
 
         for party in info["participants"]:
             self.participants[party["summonerName"]] = party
-
+            self.teams[str(party["teamId"])].append(party["summonerName"])
     
     # participants
     def getSummoner(self, name, data, default=None): return Player(self.participants.get(name), data) if self.participants.get(name) else default
 
     # info
     def getInfo(self):
-        return {"start": self.startTimeStamp, "duration": self.duration, "mode": self.gameMode}
+        return {"start": self.startTimeStamp, "duration": self.duration, "mode": self.gameMode, "map": self.mapId}
